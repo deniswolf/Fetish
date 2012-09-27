080b02aa47494f31c5dd6a2abe4671ae8028af53
DW <dear.mr.dw@gmail.com>
2012-09-28 00:56:01 +0200

    jade instead of ejs

--- config/environments/all.js
+++ config/environments/all.js
@@ -12,10 +12,10 @@ module.exports = function() {
   // Configure application settings.  Consult the Express API Reference for a
   // list of the available [settings](http://expressjs.com/api.html#app-settings).
   this.set('views', __dirname + '/../../app/views');
-  this.set('view engine', 'ejs');
+  this.set('view engine', 'jade');
 
   // Register EJS as a template engine.
-  this.engine('ejs', require('ejs').__express);
+  this.engine('jade', require('jade').__express);
 
   // Override default template extension.  By default, Locomotive finds
   // templates using the `name.format.engine` convention, for example
