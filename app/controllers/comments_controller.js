e18d6b904ce77b6f0d9ad4eec928682095017ce1
Denis Wolf <denisw@wix.com>
2012-10-03 11:56:41 +0200

    Comments in Entity: CRUD Views and Controller

--- app/controllers/comments_controller.js
+++ app/controllers/comments_controller.js
@@ -12,6 +12,7 @@ C.index = function(){
 		entity_id,
 		function(err, data){
 			if (err) self.render();
+			self.entity_id = entity_id;
 			self.comments = data.comments;
 			self.render();
 		}
@@ -26,12 +27,12 @@ C.create = function(){
 	Model.findById(
 		entity_id,
 		function(err, data){
-			if (err) self.redirect('/entities');
+			if (err) self.redirect('/entities/'+entity_id+'/comments');
 			data.comments.push(comment);
 
 			data.save(function(err){
 				if (err) return handleError(err);
-				self.redirect('/entities/'+entity_id);
+				self.redirect('/entities/'+entity_id+'/comments');
 			});
 		}
 	);
@@ -39,36 +40,42 @@ C.create = function(){
 
 C.destroy = function(){
 	var self = this,
+		entity_id = self.param('entity_id'),
 		id = self.param('id');
 
 	if (id){
-		Model.findByIdAndRemove(
-			id,
-			function(err){
+		Model.findById(
+			entity_id,
+			function(err, data){
 				if (err) return handleError(err);
-				self.redirect('/entities/');
-			}
-			);
+				self.entity_id = entity_id;
+				self.comment = data.comments.id(id).remove();
+				data.save(function(err){
+					self.redirect('/entities/'+entity_id+'/comments');
+				});
 
+			});
 	} else {
-		self.redirect('/entities/');
+		self.redirect('/entities/'+entity_id+'/comments');
 	}
 };
 
 C.show = function(){
 	var self = this,
+		entity_id = self.param('entity_id'),
 		id = self.param('id');
 
 	if (id){
 		Model.findById(
-			id,
+			entity_id,
 			function(err, data){
 				if (err) return handleError(err);
-				self.entity = data;
+				self.entity_id = entity_id;
+				self.comment = data.comments.id(id);
 				self.render();
 			});
 	} else {
-		self.render('index');
+		return;
 	}
 };
 
