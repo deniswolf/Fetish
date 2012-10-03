74b94ee156ddfaed8aada425a19bd13789add449
Denis Wolf <denisw@wix.com>
2012-10-03 18:01:12 +0200

    Enity: move all JS in layout.js

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -9,5 +9,47 @@ $(document).ready(function(){
 	})();
 
 
+	//ajax get/post sugar for Enitites and their Comments
+
+	function getUpdateContainer(){
+		var event = arguments[0];
+		event.stopPropagation();
+
+		var $container = $(this),
+			action = $container.attr('data-action');
+
+		$.ajax({
+			url: action,
+			dataType: 'html'
+		}).success(function(data){
+				$container.html(data);
+		});
+	}
+
+	function postFormUpdateContainer(){
+		var event = arguments[0];
+		event.preventDefault();
+
+		var $form = $(this),
+			$container = $form.closest('.container'),
+			postData = $form.serialize(),
+			postUrl = $form.attr('action');
+
+		$.post(postUrl, postData).success(function(){
+			$container.trigger('update');
+		});
+	}
+
+	$('body').on('update','.comments.container,.entity.container',getUpdateContainer);
+
+	$('body').on('submit','form.comments-form, form.button_to.delete',postFormUpdateContainer);
+
+	$('body').on('submit','form.select_to',postFormUpdateContainer);
+	$('body').on('change','.select_to select',function(){
+		$(this).closest('form').submit();
+	});
+
+
+
 });
 })(jQuery);
