7793fea23d09a741e556d464262d77dc37fa71fc
Denis Wolf <dear.mr.dw@gmail.com>
2013-01-16 00:56:13 +0200

    prettify programmatic loader file

--- dev.js
+++ dev.js
@@ -1,4 +1,8 @@
-var sys = require('sys')
-var exec = require('child_process').exec;
-function puts(error, stdout, stderr) { sys.puts(stdout) }
+var sys = require('sys'),
+    exec = require('child_process').exec;
+
+function puts (error, stdout, stderr) {
+  sys.puts(stdout);
+}
+
 exec("lcm server", puts);
